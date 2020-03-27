078c8ecd758b91620a0ed9a5ec7ed2d546ced089
Santosh <Santosh@LAPTOP-T07B1BV6>
2020-03-27 11:39:13 +0530

    Test pull

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,6 +18,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		
+		System.out.println();
 	/*	try {
 			FileInputStream fis=new FileInputStream(new File("C:\\Users\\Santosh\\Documents\\workspace-sts-3.9.4.RELEASE\\demo\\src\\main\\resources\\example.properties"));
 			Properties prop=new Properties();
